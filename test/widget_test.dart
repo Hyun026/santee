d46652f0362f8010c93ce6314b0937d27ecf47de
hyun <tyusra26@gmail.com>
2024-06-27 16:36:45 +0200

    master

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,18 +13,20 @@ import 'package:sante_en_poche/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( const MyApp(isConnected: false,));
+   testWidgets('MyApp shows MainScreen when connected', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp(isConnected: true));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byType(MainScreen), findsOneWidget);
+    expect(find.byType(NoConnectionScreen), findsNothing);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  testWidgets('MyApp shows NoConnectionScreen when not connected', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp(isConnected: false));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(MainScreen), findsNothing);
+    expect(find.byType(NoConnectionScreen), findsOneWidget);
+  });
+
+   
   });
 }
